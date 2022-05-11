07306cc8fd89291b14e0a9c042f6d2f5f55af960
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-05-11 08:40:41 -0700

    Create fmriprep_functionalRuns.sh

---
+++ preprocessing/scripts/fmriprep_functionalRuns.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ABOUT THIS SCRIPT
+#
+# 
+# Ian Richard Ferguson | Stanford University
+
+# === User input parameters ===
+SUBJ=$1                                                                         # Read in from command line
+THREADS=16
+MEM=30
+DUMMY_SCANS=2
+
+BIDS_ROOT="/oak/stanford/groups/jzaki/scp_2022/bids"                            # Path to BIDS data
+IMAGE=/"oak/stanford/groups/jzaki/zaki_images/fmriprep-20.2.1.simg"             # Path to singularity image
+SURFER="/oak/stanford/groups/jzaki/zaki_images/FS_LICENSE.txt"                  # Path to FreeSurfer license
+TFLOW="$HOME/.cache/templateflow"                                               # Path to templateflow sub-dir
+mkdir -p $TFLOW                                                                 # Make templateflow dir if it doesn't exist
+
+
+# === Run Singularity script ===
+singularity run --home $HOME --cleanenv $IMAGE          \
+  $BIDS_ROOT $BIDS_ROOT/derivatives                     \
+  participant                                           \
+  --participant-label $SUBJ                             \
+  --task-id faces stressbuffer socialeval               \
+  --md-only-boilerplate                                 \
+  --fs-license-file $SURFER                             \
+  --output-spaces                                       \
+      MNI152NLin6Asym:res-2                             \
+  --nthreads $THREADS                                   \
+  --stop-on-first-crash                                 \
+  --mem_mb $MEM                                         \
+  --dummy-scans $DUMMY_SCANS
